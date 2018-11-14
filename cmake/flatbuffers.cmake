6951d101e77f0211a3745089e57bc91c418abde5
SusinMat <57694766+SusinMat@users.noreply.github.com>
2018-11-14 17:23:12 -0200

    Switch from using flatbuffers v1.7.1 to using v1.9.0

--- cmake/flatbuffers.cmake
+++ cmake/flatbuffers.cmake
@@ -5,8 +5,8 @@ set(flatbuffers_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/flatbuffers)
 ExternalProject_Add(
     flatbuffers
     PREFIX ${flatbuffers_PREFIX}
-    URL "https://github.com/google/flatbuffers/archive/v1.7.1.tar.gz"
-    URL_MD5 "81934736f31fbd2cfdb513e71b53b358"
+    URL "https://github.com/google/flatbuffers/archive/v1.9.0.tar.gz"
+    URL_HASH SHA256=5ca5491e4260cacae30f1a5786d109230db3f3a6e5a0eb45d0d0608293d247e3
     CMAKE_ARGS -DCMAKE_CXX_FLAGS=-Wno-ignored-qualifiers -DCMAKE_INSTALL_PREFIX=${flatbuffers_PREFIX} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     LOG_UPDATE ON
     LOG_CONFIGURE ON
